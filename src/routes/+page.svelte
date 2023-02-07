82f59539dc952d33445258bd7826b94f85f18619
Pooh <purichseenuallae@gmail.com>
2023-02-08 00:10:52 +0700

    add nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,19 @@
 <script>
-    import Home from "./home.svelte";
+    import { count } from '../store.js';
+    import Home_desktop from "./home_desktop.svelte";
+    import Navbar from "./navbar.svelte";
+    /**
+     * @type {number}
+     */
+    let countValue;
+
+    count.subscribe((value) => {
+        countValue = value;
+    });
 </script>
+
 <div>
-    <Home />
+    <Navbar />
+    <Home_desktop />
+    
 </div>
---
+++ src/routes/navbar.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { count } from "../store.js";
+
+    let bg_color = "#7209B7";
+
+    function home() {
+        count.set(0);
+        bg_color = "#7209B7";
+    }
+    function about() {
+        count.set(1);
+        bg_color = "#000000";
+    }
+    function contact() {
+        count.set(2);
+        bg_color = "#000000";
+    }
+</script>
+
+<div class="navbar" style="background-color: {bg_color}">
+    <button on:click={home}> Home</button>
+    <button on:click={about}> About </button>
+    <button on:click={contact}> Contact </button>
+</div>
+
+<style>
+    .navbar {
+        color: #fff;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 24px;
+        line-height: 36px;
+        text-align: center;
+    }
+</style>
